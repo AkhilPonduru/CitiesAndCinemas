6a5537dbe5a1ee67d9df77574e90264d0d0dd896
Akhil POnduru <akhil@codeft.io>
2021-07-09 12:50:11 +0530

    Added GET Requests

--- utils/app.js
+++ utils/app.js
@@ -1,55 +1,78 @@
 const express = require('express');
 const fs = require('fs');
 
-const data = JSON.parse(
-    fs.readFileSync("cinemas.json")
-);
-
 // Require logger.js
-const logger = require('./logger');
+const logger = require('../utils/logger');
 const app = express();
 const port = 3000;
 const host = "localhost";
 
-// Dummy Express GET call
+const data = JSON.parse(
+    fs.readFileSync("cinemas.json")
+);
+const place = JSON.parse(
+    fs.readFileSync("areas.json")
+);
+
 app.get('/cinemas',(req,res) => {
     res.status(200).json(data);
     logger.info("Server started and running succesfully");
 });
 
-// Introduce error by using undefined variable 'y'
 app.get('/cinemas/:city',(req,res) => {
-    const city = req.params.city;  //This is to make id a number from string
+    const city = req.params.city; 
     const cities = data.find(el =>
-        el.city === city //this is to compare the id in the given data 
-                             //and to provide the required output 
-    )
+        el.city === city 
+        )
     res.status(200).json(cities);
-})
+    logger.info("Server running good and somebody got access to ciities");
+});
 
 app.get('/cinemas/:city/:area',(req,res) => {
     const area = req.params.area;
-    const center = 0
-    for(var i=0;i<data.length;i++){
-        for(var j=0;j<data[i].Area.length;j++){
-            if(data[i].Area[j] === area){
-                console.log(data[i].Area[j])
-                // center = data[i].Area[j]
-            }
+    const city = req.params.city;
+
+    var center = 0;
+    for(var i in place){
+        if(i == area){
+            center = place[i]
+        }
+    };
+    res.status(200).json({
+        City : city,
+        Area : area,
+        Screens : center
+    });
+    logger.info("Server running well and someone got access to city and screens in it");
+});
+
+app.get('/cinemas/:city/:area/:screen',(req,res) => {
+    const area = req.params.area;
+    const city = req.params.city;
+    const screen = req.params.screen
+
+    var center = 0;
+    for(var i in place){
+        if(i == area){
+            center = place[i]
+        }
+    };
+
+    // var Shows = JSON.parse(center)
+    var RequiredScreen = null
+    for(var j in center){
+        if(j == screen){
+            RequiredScreen = center[j]
         }
     }
-    // for(var i=0;i<data.length;i++){
-    //     var center = data[i].Area.find(el =>
-    //         el.Area === area)
-    // }
-    res.status(200).json(center);
-})
+    res.status(200).json({
+        City : city,
+        Area : area,
+        Screen : RequiredScreen
+    });
+    logger.info("Server running well and someone got access to city and a particular screen in it");
+});
 
-// Capture 404 erors
-app.use((req,res,next) => {
-    res.status(404).send("PAGE NOT FOUND");
-    logger.error(`400 || ${res.statusMessage} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
-})
 // Run the server
 app.listen(port, () => {
     console.log("Server started...");
